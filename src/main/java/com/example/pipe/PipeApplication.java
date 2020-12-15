7668e34a72cac8f26bda8fd4200125eed1e40614
yossi78 <yossi78@gmail.com>
2020-12-15 14:14:25 +0200

    First commit

---
+++ src/main/java/com/example/pipe/PipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipeApplication.class, args);
+    }
+
+}
